28f857c6a7867622903c1f7418e0d29a4159cf7c
svnrnns <haruueel@gmail.com>
2025-06-12 13:49:06 +0200

    feat: animated onlongpress habit card

--- src/features/main/habits/HabitCard.tsx
+++ src/features/main/habits/HabitCard.tsx
@@ -3,6 +3,7 @@ import LucideIcon from '@/lib/LucideIcon';
 import * as Haptics from 'expo-haptics';
 import { useCallback } from 'react';
 import { Pressable, Text, View } from 'react-native';
+import Animated, { useSharedValue, withSpring } from 'react-native-reanimated';
 
 interface Props {
   habit: Habit;
@@ -15,35 +16,59 @@ const frequencyRelationship: {
   weekly: 'weeks',
   monthly: 'months',
 };
+
+const vibrate = async () => {
+  await Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Soft);
+};
+
 export default function HabitCard({ habit }: Props) {
-  const vibrate = useCallback(async () => {
-    await Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Soft);
-  }, []);
+  const scale = useSharedValue(1);
+
+  const enterBlink = useCallback(() => {
+    scale.value = withSpring(1.05);
+  }, [scale]);
+
+  const exitBlink = useCallback(() => {
+    scale.value = withSpring(1);
+  }, [scale]);
+
   return (
     <Pressable
-      onPress={vibrate}
-      className="w-full mb-3 p-4 rounded-2xl bg-module flex flex-col shadow-[0_0_14px_rgba(0,0,0,0.05)]"
+      onLongPress={async () => {
+        enterBlink();
+        // editHabit
+        await vibrate();
+      }}
+      onPressOut={() => {
+        exitBlink();
+      }}
     >
-      <Text className="text-lg text-heading font-medium">{habit.title}</Text>
-      <Text className="text-sm text-placeholder font-medium">
-        {habit.description}
-      </Text>
-      <View className="flex flex-row items-center gap-1.5 mt-3">
-        <View className="px-2 py-1 rounded-lg bg-main/20">
-          <Text className="text-sm text-main font-medium">
-            {habit.frequency.toUpperCase()}
-          </Text>
+      <Animated.View
+        style={{ transform: [{ scale }] }}
+        className="w-full mb-3 p-4 rounded-2xl bg-module flex flex-col shadow-[0_0_14px_rgba(0,0,0,0.05)]"
+      >
+        <Text className="text-lg text-heading font-medium">{habit.title}</Text>
+        <Text className="text-sm text-placeholder font-medium">
+          {habit.description}
+        </Text>
+        <View className="flex flex-row items-center gap-1.5 mt-3">
+          <View className="px-2 py-1 rounded-lg bg-main/20">
+            <Text className="text-sm text-main font-medium">
+              {habit.frequency.toUpperCase()}
+            </Text>
+          </View>
+          <View className="w-fit px-2 py-1 gap-1 rounded-lg border border-warning flex flex-row items-center justify-between">
+            <LucideIcon
+              name="Flame"
+              className="size-4 text-warning"
+            />
+            <Text className="text-sm text-warning font-medium">
+              {habit.streak_count} {frequencyRelationship[habit.frequency]}{' '}
+              streak
+            </Text>
+          </View>
         </View>
-        <View className="w-fit px-2 py-1 gap-1 rounded-lg border border-warning flex flex-row items-center justify-between">
-          <LucideIcon
-            name="Flame"
-            className="size-4 text-warning"
-          />
-          <Text className="text-sm text-warning font-medium">
-            {habit.streak_count} {frequencyRelationship[habit.frequency]} streak
-          </Text>
-        </View>
-      </View>
+      </Animated.View>
     </Pressable>
   );
 }
